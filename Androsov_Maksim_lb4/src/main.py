1a7483d93867fd5aa932eaeb50e5d1d7574959d1
Androsss1 <and12_06@mail.ru>
2026-05-04 22:47:03 +0300

    updated

--- Androsov_Maksim_lb4/src/main.py
+++ Androsov_Maksim_lb4/src/main.py
@@ -1,18 +1,19 @@
-
-
 def prefix_func(p):
     n = len(p)
     arr = [0] * n
-    for i in range(1,n):
-        j = arr[i-1]
-        while j > 0 and p[i] !=p[j]:
-            j = arr[j-1]
-        if p[i] == p[j]: j +=1 
-        arr[i] = j 
 
-    return arr 
+    for i in range(1, n):
+        j = arr[i - 1]
+        while j > 0 and p[i] != p[j]:
+            j = arr[j - 1]
+        if p[i] == p[j]:
+            j += 1
+        arr[i] = j
 
-
+    print("\n prefix array")
+    print(*arr)
+    print(" ")
+    return arr
 
 
 def kmp_search(p, t):
@@ -23,27 +24,31 @@ def kmp_search(p, t):
 
     j = 0
     for i in range(n):
+        print(f"i={i}, j={j}, t[i]='{t[i]}', p[j]='{p[j]}'")
         while j > 0 and t[i] != p[j]:
-            j = arr[j-1]
-        
-        if t[i] == p[j]: 
-            j +=1 
+            j = arr[j - 1]
+            print(f"  j shifted to {j}")
 
-        if j == m: 
+        if t[i] == p[j]:
+            j += 1
+            print(f"  match, j={j}")
+        else:
+            print(f"  no match")
+
+        if j == m:
             ans.append(i - m + 1)
-            j = arr[j-1]
-    
+            print(f"  found match at index {i - m + 1}")
+            j = arr[j - 1]
+            print(f"  j reset to {j}")
+
     return ans
-
-
-
 
 
 if __name__ == "__main__":
     p = input().strip()
-    t = input().strip() 
-    ans = kmp_search(p,t)
-    if len(ans) == 0: print(-1)
-    else: print(",".join(map(str,ans)))
-
-
+    t = input().strip()
+    ans = kmp_search(p, t)
+    if len(ans) == 0:
+        print(-1)
+    else:
+        print(",".join(map(str, ans)))
